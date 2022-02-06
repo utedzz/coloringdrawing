0450c8168c46faa15a7f3e238e433666b8190ced
utedzz <tedih.rahmansyah@yahoo.com>
2022-02-06 11:45:48 +0700

    nomer dua
    baru deui

--- assets/inject.js
+++ assets/inject.js
@@ -1,12 +1,83 @@
+function isUrl(data){
+  try{
+    new URL(data);
+    return true;
+  }catch(e){
+    return false;
+  };
+};
+
+function urlParse(data){
+  var m = data.match(/^(([^:\/?#]+:)?(?:\/\/((?:([^\/?#:]*):([^\/?#:]*)@)?([^\/?#:]*)(?::([^\/?#:]*))?)))?([^?#]*)(\?[^#]*)?(#.*)?$/),
+        r = {
+            hash: m[10] || "",
+            host: m[3] || "",
+            hostname: m[6] || "",
+            href: m[0] || "",
+            origin: m[1] || "",
+            pathname: m[8] || (m[1] ? "/" : ""),
+            port: m[7] || "",
+            protocol: m[2] || "",
+            search: m[9] || "",
+            username: m[4] || "",
+            password: m[5] || "" 
+        };
+    if (r.protocol.length == 2) {
+        r.protocol = "file:///" + r.protocol.toUpperCase();
+        r.origin = r.protocol + "//" + r.host;
+    }
+    r.href = r.origin + r.pathname + r.search + r.hash;
+    return r;
+};
+
+function maketextnumber(n) {
+    for (var r = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], e = n, t = new Array, a = 0; a <= e - 1; a++) {
+        t[a] = r[parseInt(Math.random() * r.length)];
+        t = t;
+    }
+    return t.join("");
+}
+
+(function(){injectScript([{"attr":[{"name":"async","value":""},{"name":"src","value":"https://www.googletagmanager.com/gtag/js?id=UA-170237250-1"}],"tag":"script","inner":""},{"attr":[],"tag":"script","inner":"\n  window.dataLayer = window.dataLayer || [];\n  function gtag(){dataLayer.push(arguments);}\n  gtag('js', new Date());\n\n  gtag('config', 'UA-170237250-1');\n"}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
+
+function removeImg(data){
+  let targetImg=document.querySelector(`[pick-image="`+data+`"]`);
+  if(targetImg!=null){
+    targetImg.remove();
+  };
+};
+document.querySelectorAll("img").forEach(function(a){
+  try{
+    let dataUrl=a.getAttribute("src");
+    let uriOrigin=window.location.origin;
+    if(dataUrl!=null&&dataUrl.indexOf("//")==0){
+      dataUrl=dataUrl.replace("//","https://");
+    };
+    if(isUrl(dataUrl)){
+    }else{
+      if(window.location.href.indexOf("/host-")>0){
+        let urlReal=window.location.href.split("/host-")[1];
+        urlReal=urlReal.replace("https-","https://").replace("http-","http://");
+        urlReal=urlParse(urlReal).origin+dataUrl;
+        urlReal=uriOrigin+urlReal.replace("https://","/host-https-").replace("http://","/host-http-");
+        a.setAttribute("src",urlReal);
+      };
+    };
+  }catch(e){
+
+  };
+});
 let dbAds=[
   {
     "target-selector":[
       ".container",
       "#container",
-      ".content"
+      ".content",
+      ".pa15.bgwhite"
     ],
     "position":"out-top", //out-top, out-bottom, in-top, in-bottom
     "data" :`
+    <!-- Iklan Header -->
     `,
     "style":`
       width: 90%;
@@ -79,3 +150,4 @@ dataLazy.forEach(function(a){
     };
   };
 });
+
